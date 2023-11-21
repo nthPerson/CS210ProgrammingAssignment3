6d9f6e24274205510d2896649e33bb7c3c81077e
Robert Ashe <123716477+nthPerson@users.noreply.github.com>
2023-11-21 12:06:58 -0800

    Fixed constructor for Graph, tested Edge, and Graph.

--- prims.cpp
+++ prims.cpp
@@ -9,20 +9,44 @@ public:
     int endVertex;
     int weight;
 
-//    Edge(int weight, int startVertex, int endVertex) : weight(weight), startVertex(weight), endVertex(endVertex);
-    Edge(int weight, int startVertex, int endVertex) {
-        this->weight = weight;
-        this->startVertex = startVertex;
-        this->endVertex = endVertex;
-    }
+    // TODO Default constructor for Edge?
+//    Edge() {
+//        startVertex = -1;
+//        endVertex = -1;
+//        weight = 0;
+//    }
+
+    Edge(int weight, int startVertex, int endVertex) : weight(weight), startVertex(startVertex), endVertex(endVertex) {};
+//    Edge(int weight, int startVertex, int endVertex) {
+//        this->weight = weight;
+//        this->startVertex = startVertex;
+//        this->endVertex = endVertex;
+//    }
 
     bool operator>(const Edge &otherEdge) const {
+//        return weight < otherEdge.weight;
         return weight > otherEdge.weight;
     }
 
-    bool operator<(const Edge &otherEdge) {
+    bool operator<(const Edge &otherEdge) const {
         return weight < otherEdge.weight;
     }
+
+    bool operator==(const Edge &otherEdge) const {
+        return weight == otherEdge.weight;
+    }
+
+    void print() {
+        // TODO ? add endline in Edge::print()
+        cout << startVertex << " -> " << endVertex << " : " << weight;
+    }
+};
+
+// not sure if this will be necessary for Prim's
+class Vertex {
+public:
+    int predecessor;
+    int minWeight;
 };
 
 class Graph {
@@ -35,28 +59,21 @@ public:
     // constructor
     Graph(int vertices) : numVertices(vertices), adjacencyMatrix(vertices, vector<int>(vertices, 0)) {}
 
-    Graph(int sampleGraph[5][5]) {
+    Graph(int vertices, int sampleGraph[][5]) : numVertices(vertices), adjacencyMatrix(vertices, vector<int>(vertices, 0)) {
+//        adjacencyMatrix(vertices, vector<int>(vertices, 0));
         // convert sample graph to Graph type to allow for more flexibility for Prim's algo input
         for (int i = 0; i < 5; ++i) {
             for (int j = 0; j < 5; ++j) {
                 this->adjacencyMatrix[i][j] = sampleGraph[i][j];
             }
         }
+//        numVertices = 5;
     }
 
     void addEdge(int startVertex, int endVertex, int weight) {
         adjacencyMatrix[startVertex][endVertex] = weight;
-        // since the graph is undirected, add reverse of edge
+        // since the graph is undirected, add reverse of top
         adjacencyMatrix[endVertex][startVertex] = weight;
-    }
-
-    void display() {
-        for (int i = 0; i < numVertices; ++i) {
-            for (int j = 0; j < numVertices; ++j) {
-                cout << adjacencyMatrix[i][j] << " ";
-            }
-            cout << endl;
-        }
     }
 
     int getNumVertices() {
@@ -88,6 +105,7 @@ public:
 template<typename T>
 class PriorityQueue {
 private:
+    T *top;
     vector<T> heap;
 
     void downHeap(int index) {
@@ -106,9 +124,6 @@ private:
         if (smallestIndex != index) {
             // swap
             swap(heap[index], heap[smallestIndex]);
-//            int temp = smallestIndex;
-//            smallestIndex = index;
-//            index = temp;
             downHeap(smallestIndex);
         }
     }
@@ -119,14 +134,17 @@ private:
         if (index && heap[parentIndex] > heap[index]) {
             // swap
             swap(heap[index], heap[parentIndex]);
-//            int temp = parentIndex;
-//            parentIndex = index;
-//            index = temp;
             upHeap(parentIndex);
         }
     }
 
 public:
+//    PriorityQueue() {
+//        top = nullptr;
+//
+//
+//    }
+
     bool isEmpty() {
         return heap.empty();
     }
@@ -160,15 +178,6 @@ public:
 
 };
 
-class Vertex {
-public:
-    int predecessor;
-    int minWeight;
-
-};
-
-
-
 void primsMST(Graph &graph) {
     int numVertices = graph.getNumVertices();
     vector<bool> visited(numVertices, false);
@@ -190,7 +199,7 @@ void primsMST(Graph &graph) {
 
     // build the rest of the MST
     while (!minEdgePQueue.isEmpty() && minSpanTree.size() < numVertices - 1) {
-        // minimum edge will always be the top of the p-queue
+        // minimum top will always be the top of the p-queue
         Edge minEdge = minEdgePQueue.getTop();
         minEdgePQueue.deleteTop();
 
@@ -215,10 +224,69 @@ int main() {
 
 
     // test PriorityQueue and Graph
-    PriorityQueue<Edge> examplePQ;
+    PriorityQueue<Edge> *pqTest;
+
+    Graph *testGraph = new Graph(5, G);
+    Edge edge1(20, 1, 2);
+    Edge edge2(20, 3, 4);
+    Edge edge3(5, 5, 6);
+    Edge edge4(10, 7, 8);
+
+    if (pqTest->isEmpty()) {
+        cout << "PQ is empty." << endl;
+    } else {
+        cout << "isEmpty does not work." << endl;
+    }
+
+    //test Edge::print()
+//    cout << "Edge1: " << endl;
+//    edge2.print();
+
+    // test addEdge()
+//    cout << "Graph before adding top:" << endl;
+//    testGraph->print();
+//
+//    cout << "Graph after adding top:" << endl;
+//    testGraph->addEdge(4, 1, 99999);
+//    testGraph->print();
+
+    // test get numVertices()
+//    cout << "Number of vertices in testGraph: " << testGraph->getNumVertices() << endl;
+
+    // test hasEdge
+//    if (testGraph->hasEdge(0, 1)) {
+//        cout << "Edge at (0, 1) confirmed." << endl;
+//        cout << "Weight of top at (0, 1): " << testGraph->getEdgeWeight(0, 1) << endl;
+//    }
 
 
+    // Compare edge1 with edge2
+//    if (edge1 > edge2) {
+//        cout << "Edge1 is greater than Edge2" << endl;
+//    } else if (edge1 == edge2) {
+//        cout << "Edge1 is equal to Edge 2" << endl;
+//    } else {
+//        cout << "Edge1 is not greater than Edge2" << endl;
+//    }
+//
+//    // Compare edge3 with edge1
+//    if (edge3 < edge1) {
+//        cout << "Edge3 is less than Edge1" << endl;
+//    } else {
+//        cout << "Edge3 is not less than Edge1" << endl;
+//    }
+//
+//    // Comparing edges with equal weights
+//    if (edge1 > edge4) {
+//        cout << "Edge1 is greater than Edge4" << endl;
+//    } else if (edge1 < edge4) {
+//        cout << "Edge1 is less than Edge4" << endl;
+//    } else {
+//        cout << "Edge1 and Edge4 are equal in weight" << endl;
+//    }
 
+
+//    testGraph->print();
 
 
 
